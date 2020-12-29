3f03cd245921e44af40004de7853ef48259dc9df
calincatalinmihai <76654633+calincatalinmihai@users.noreply.github.com>
2020-12-29 16:43:41 +0200

    Add files via upload

--- Database.h
+++ Database.h
@@ -1,6 +1,8 @@
+#pragma once
 #define Database_H
 #include <string>
 #include <vector>
+#include "Table.h"
 using namespace std;
 
 class Database {
@@ -11,11 +13,46 @@ public:
 	string validateCreate(string& str);
 	string validateDrop(string& str);
 	string validateDisplay(string& str);
+
+	~Database();
+	//void adaugaTable(Table*);
+
+	//friend istream& operator >> (istream&, Database&);
+	//friend ostream& operator << (ostream&, Database&);
+	Database& operator = (const Database&);
+	Database(const Database&);
+	bool operator!();
+	Database operator++();
+	Database operator++(int);
+	Database operator+(int);
+	Table& operator[](int);
+	explicit operator int();
+	int operator()();
+
+	int getnbOfTables();
+	void setnbOfTables(int);
+	Table* gettables();
+	void settables(Table*, int);
+	void setname(const char*);
+	char* getname();
+	Table gettables(int);
+	int compara(Database*);
+	bool less(Database&);
+	bool operator< (Database&);
+	bool operator==(Database&);
+
 private:
+	static Database* instance;
+	//vector<Table> tables;
+	char* name;
+	Table* tables;
+	int nbOfTables;
+
+	Database();
+	Database(char*, Table*, int);
+
 	int commandType; /// 1 pt create, 2 pt drop, 3 pt display
 	vector<string> commandParams; /// vector de string de parametrii
 	string commandTableName;
-	static Database* instance;
-	//vector<Table> tables;
 
 };
